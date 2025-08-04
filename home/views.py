658da817c622fbb00bc4036e6f5dea4a9685d560
Tejaswini uggina <tejaswiniuggina282@gmail.com>
2025-08-04 23:50:43 +0530

    21116

--- home/views.py
+++ home/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import ContactForm
 
 # Create your views here.
+def contact_us(request):
+    if request.method=='POST':
+        form=ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('contact_us')
+    else:
+        form=ContactForm()
+    return render(request,'home/contact_us.html',{'form':form})
