fe7955503400405033842b291182c5e6926d8eb6
Tejaswini uggina <tejaswiniuggina282@gmail.com>
2025-08-06 23:20:45 +0530

    21118

--- products/models.py
+++ products/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class MenuItem(models.Model):
+    name=models.CharField(max_length=200)
+    description=models.TextField()
+    price=models.DecimalField(max_digits=6,decimal_places=2)
+
+    def __str__(self):
+        return self.name
 class Item(models.Model):
     item_name = models.CharField(max_length=150)
     item_price = models.DecimalField(max_digits=10, decimal_places=2)
