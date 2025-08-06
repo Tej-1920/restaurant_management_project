19d6a733bc48a0cf3e6de5614024ad7767520880
Tejaswini uggina <tejaswiniuggina282@gmail.com>
2025-08-06 23:31:10 +0530

    21119

--- products/urls.py
+++ products/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('items/', ItemView.as_view(), name='item-list'),
     path('menu/',MenuView.as_view()),
     path('',show_menu,menu='show_menu'),
-    path('menus/',m)
+    path('menus/',menuview)
 ]
