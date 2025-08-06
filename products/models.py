92072e65a56c4723ef896194e2688f988b1fcf3c
Tejaswini uggina <tejaswiniuggina282@gmail.com>
2025-08-06 23:21:22 +0530

    21118

--- products/models.py
+++ products/models.py
@@ -8,6 +8,7 @@ class MenuItem(models.Model):
 
     def __str__(self):
         return self.name
+        
 class Item(models.Model):
     item_name = models.CharField(max_length=150)
     item_price = models.DecimalField(max_digits=10, decimal_places=2)
