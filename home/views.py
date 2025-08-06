9b6b27c8585002bc4c03030fa4c2bc963335c085
Tejaswini uggina <tejaswiniuggina282@gmail.com>
2025-08-06 23:47:19 +0530

    21120

--- home/views.py
+++ home/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render,redirect
 from .forms import ContactForm
 from .models import Restaurant
 from .serializers import RestaurantSerializer
-
+from django.conf import settings
 # Create your views here.
+def home_view(request):
+    restaurant_name=settings.RESTAURANT_NAME
+    return render(request,'home/home.html',{'restaurant_name':restaurant_name})
+    
 class RestaurantSerializerView(APIView):
     def get(self,request):
         restaurant=Restaurant.objects.first()
