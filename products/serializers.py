005110bf690545542cdf9fc08fe03347c9ccc550
Tejaswini uggina <tejaswiniuggina282@gmail.com>
2025-08-21 16:35:36 +0530

    35003

--- products/serializers.py
+++ products/serializers.py
@@ -1,7 +1,11 @@
 from rest_framework import serializers
-from .models import Item
+from .models import Item,MenuItem
 
 class ItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Item
         fields = '__all__'
+
+class MenuItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=
