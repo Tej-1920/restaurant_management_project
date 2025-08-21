41a02593c3a28f63cb8053b12c09a6e64eafaf39
Tejaswini uggina <tejaswiniuggina282@gmail.com>
2025-08-21 20:39:13 +0530

    35005

--- home/models.py
+++ home/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    name=forms.CharField(max_length=50,label="Enter name")
+    email=forms.EmailField(label="Enter email")
+    message=forms.CharField(widget=forms.Textarea,label="Message")
