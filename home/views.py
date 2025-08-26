943d1c1b059b3eeb49d185eeecc4a3e50a4fb7ab
Tejaswini uggina <tejaswiniuggina282@gmail.com>
2025-08-26 08:06:54 +0530

    39175

--- home/views.py
+++ home/views.py
@@ -3,6 +3,7 @@ from .forms import ContactForm
 from .models import Restaurant
 from .serializers import RestaurantSerializer
 from django.conf import settings
+from datetime import datetime
 # Create your views here.
 
 class RestaurantSerializerView(APIView):
@@ -24,7 +25,8 @@ def contact_us(request):
 def home_view(request):
     restaurant_name=settings.RESTAURANT_NAME
     restaurant_phone_number=settings.RESTAURANT_PHONE_NUMBER
-    return render(request,'home/home.html',{'restaurant_name':restaurant_name,'phone_number':restaurant_phone_number})
+    current_year = datetime.now().year
+    return render(request,'home/home.html',{'restaurant_name':restaurant_name,'phone_number':restaurant_phone_number,'year':current_year})
 
 def about_us(request):
     return render(request,'about_us.html')
